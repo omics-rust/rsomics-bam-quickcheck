152c2cc02f363b0c8b50bdff7c2f4af826496014
Bengerthelorf <bengerlorf@hotmail.com>
2026-05-29 05:19:42 +0800

    test(bam-quickcheck): add criterion bench harness

---
+++ benches/bench.rs
@@ -0,0 +1,22 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use std::hint::black_box;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn bench_bam_quickcheck(c: &mut Criterion) {
+    let bin = env!("CARGO_BIN_EXE_rsomics-bam-quickcheck");
+    let manifest = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let bam = manifest.join("tests/golden/ok.bam");
+    c.bench_function("rsomics-bam-quickcheck golden", |b| {
+        b.iter(|| {
+            let out = Command::new(black_box(bin))
+                .arg(bam.to_str().unwrap())
+                .output()
+                .unwrap();
+            assert!(out.status.success());
+        });
+    });
+}
+
+criterion_group!(benches, bench_bam_quickcheck);
+criterion_main!(benches);
